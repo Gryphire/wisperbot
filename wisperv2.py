91392310d163a4a76bed18318137c12475811373
Bryan Kam <github@vo.racio.us>
2024-01-25 13:45:34 +0000

    Working very hacky prototype that uses database and runs through tutorial

---
+++ wisperv2.py
@@ -0,0 +1,331 @@
+#!/usr/bin/python
+
+import os
+import json
+import logging
+import random
+import sqlite3
+import subprocess
+from datetime import datetime
+import dotenv
+import openai
+from telegram import Update
+from telegram.error import TimedOut
+from telegram.ext import (filters, MessageHandler, ApplicationBuilder,
+  CommandHandler, ContextTypes, CallbackContext, ConversationHandler)
+
+dotenv.load_dotenv()
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+## Set up for conversation handler
+GET_PARTICIPANT_NUMBER = 0
+DB = "wisper.db"
+#CONFIRM_PARTICIPANT_NUMBER = 1
+
+## LOGGER FILE SETUP
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+class HTTPXFilter(logging.Filter):
+    '''Filter out lines starting with HTTP'''
+    def filter(self, record):
+        return not record.msg.startswith("HTTP")
+
+top_level_logger = logging.getLogger("top_level")
+top_level_handler = logging.FileHandler("central_log.log")
+top_level_formatting = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+top_level_handler.setFormatter(top_level_formatting)
+top_level_logger.setLevel(logging.INFO)
+top_level_logger.addHandler(top_level_handler)
+top_level_logger.addFilter(HTTPXFilter())
+top_level_logger.propagate = False
+
+# Save logger to object that we can call in future to save events to
+logging.getLogger("httpx").addFilter(HTTPXFilter())
+# Keep a dictionary of loggers:
+chat_handlers = {}
+
+try:
+    os.makedirs('sent')
+except FileExistsError:
+    pass
+
+class ChatHandler:
+    def __init__(self, chat_id, update=None, context=None):
+        self.chat_id = chat_id
+        self.chat_type = update.message.chat.type
+        self.context = context
+        self.sent = []
+        if self.chat_type == 'private':
+            self.name = update.message.from_user.full_name
+            self.first_name = update.message.from_user.first_name
+        elif 'group' in self. chat_type: # To inlude both group and supergroup
+            self.name = update.message.chat.title
+        try:
+            with open(f'chat_sessions/chat-{self.chat_id}', 'r', encoding='utf-8') as f:
+                self.number = int(f.read())
+        except FileNotFoundError:
+            self.number = None
+        self.logger = self.get_logger()
+        self.load_sent_files()
+
+    @property
+    def directory(self):
+        part = f'{self.chat_type}/{self.name}'
+        try:
+            os.makedirs(f'{part}')
+        except FileExistsError:
+            pass
+        return part
+
+    def load_sent_files(self):
+        filename = f'sent/{self.chat_id}.json'
+        try:
+            with open(filename) as f:
+                self.sent = json.load(f)
+            self.log('Successfully loaded list of sent files')
+        except (FileNotFoundError, json.decoder.JSONDecodeError):
+            self.sent = []
+
+    def save_sent_files(self):
+        filename = f"sent/{self.chat_id}.json"
+        with open(filename, 'w') as f:
+            json.dump(self.sent, f)
+
+    def get_logger(self):
+        log_file = f'{self.directory}/chat_{self.chat_id}'
+        formatting = formatting = logging.Formatter(
+                '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler = logging.FileHandler(log_file)
+        handler.setFormatter(formatting)
+        logger = logging.getLogger(f"chat-{self.chat_id}")
+        logger.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        logger.info(f'Now logging to {log_file}')
+        return logger
+
+    def log(self, msg):
+        '''Log a message to the correct log file'''
+        self.logger.info(msg)
+        top_level_logger.info(f"chat-{self.chat_id} {self.name}: {msg}")
+
+    async def choose_random_vn(self):
+        self.log(f'Sending random voicenote to {self.name}')
+        exclude = str(self.chat_id)
+        ogg_files = [f for f in os.listdir() if f.endswith('.ogg')]
+        #Uncomment this so we don't send people's voicenotes back to them:
+        ogg_files = [f for f in ogg_files if f not in self.sent and exclude not in f]
+        try:
+            random_file = random.choice(ogg_files)
+            self.sent.append(random_file)
+            self.save_sent_files()
+            self.log(f'Selected random voicenote: {random_file}')
+            return random_file
+        except IndexError:
+            await self.send_msg("""Exciting! You've listened to all the reflections I've got for you so far.
+
+Please run /??? to enter the main experience!""")
+            self.log(f'No suitable voicenotes to choose from.')
+            return None
+
+    async def send_msg(self, text, reply_markup = None):
+        '''Send a message in this chat; try infinitely'''
+        while True:
+            try:
+                # Make the Telegram request
+                await self.context.bot.send_message(
+                    self.chat_id, text, parse_mode='markdown', reply_markup=reply_markup
+                )
+                # Request succeeded, break the loop
+                break
+            except TimedOut:
+                # Timeout error occured, log a warning
+                self.logger.warning("Request timed out, retrying...")
+
+                # Wait a few seconds before retrying
+                await asyncio.sleep(5)
+
+    async def send_vn(self,VN):
+        '''Send a voicenote file'''
+        c.execute("INSERT INTO logs VALUES (?,?,?,?)", 
+                  (datetime.now(),
+                   self.name, 
+                   self.chat_id, 
+                   VN))
+        conn.commit()
+        self.sent.append(VN)
+        self.save_sent_files()
+        await self.context.bot.send_voice(chat_id=self.chat_id, voice=VN)
+        self.log(f'Sent {VN}')
+
+    async def send_random_vn(self):
+        random_vn = await self.choose_random_vn()
+        if random_vn:
+            await self.send_msg(f"Here's a response from another participant:")
+            await self.send_vn(random_vn)
+            await self.send_msg("Listen to another by typing /request")
+
+    async def sqlquery(self,cmd,fetchall=False):
+        c.execute(cmd)
+        if fetchall:
+            result = c.fetchall()
+        else:
+            result = c.fetchone()
+        return result
+
+    async def send_intro_vn(self):
+        # Check what voice notes have been sent
+        all_sent = True
+        for i in range(1,5):
+            vn = f'value{i}.ogg'
+            r = await self.sqlquery(f'SELECT * FROM logs WHERE filename="{vn}"')
+            if not r:
+                await self.send_msg(f"Here's a message for you to listen to:")
+                await self.send_vn(vn)
+                await self.send_msg(f"So, __what's the rub__? Which driving forces underlie the storyteller's experience?")
+                all_sent = False
+                break
+            else:
+                continue
+        if all_sent:
+            await self.send_msg(f"Well done! You have now listened to all of the responses. Please run /??? to enter the main sequence")
+
+    async def transcribe(self,filename):
+        txt = f"{filename}.txt"
+        webm = 'temp.webm'
+        cmd = f'ffmpeg -i file:"{filename}" -c:a libopus -b:a 64k -af atempo=2 -y {webm}'
+        subprocess.check_output(cmd, shell=True)
+        audio_file = open(webm,'rb')
+        try:
+            transcript = openai.Audio.transcribe('whisper-1',audio_file).pop('text')
+            with open(txt,"w",encoding="utf-8") as f:
+                f.write(transcript)
+            self.log(f"Transcribed {filename} to {filename}.txt")
+            os.unlink(webm)
+            return transcript
+        except openai.error.InvalidRequestError:
+            return None
+        #cmd = f'rclone copy --drive-shared-with-me -P 00_Participants bryankam8@gmail.com:"04_AUDIO PROTOTYPE_June 2023/00_Participants"'
+        #subprocess.check_output(cmd, shell=True)
+
+
+async def chunk_msg(msg):
+    chunks = []
+    current_chunk = ""
+    for word in msg.split(" "):
+        if len(current_chunk + word) > 4096:
+            chunks.append(current_chunk)
+            current_chunk = ""
+
+        current_chunk += word + " "
+    if current_chunk != "":
+        chunks.append(current_chunk)
+    return chunks
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''Given a message, echo it back with their name'''
+    chat = await initialize_chat_handler(update,context)
+    chat.log(f'{update.message.text}')
+    await chat.send_msg(f'Hi {chat.first_name}! Please run /start for instructions. 😊')
+
+async def get_voice(update: Update, context: CallbackContext) -> None:
+    chat = await initialize_chat_handler(update,context)
+    part = chat.directory
+    # get basic info about the voice note file and prepare it for downloading
+    new_file = await context.bot.get_file(update.message.voice.file_id)
+    # download the voice note as a file
+    ts = datetime.now().strftime("%Y%m%d-%H:%M")
+    filename = f"{ts}-{update.message.from_user.first_name}-{chat.chat_id}-{new_file.file_unique_id}.ogg"
+    await new_file.download_to_drive(filename)
+    chat.log(f"Downloaded voicenote as {filename}")
+    await chat.send_msg(f"Thank you for recording, {chat.first_name}!")
+    transcript = await chat.transcribe(filename)
+    # Uncomment the following if you want people to receive their transcripts:
+    #chunks = await chunk_msg(f"Transcription:\n\n{transcript}")
+    #for chunk in chunks:
+    #    await context.bot.send_message(
+    #        chat_id=update.effective_chat.id,
+    #        text=chunk
+    #    )
+    await chat.send_random_vn()
+
+#def save_start_time(start_time, part_id):
+#    ''' Save start time in seconds to start_time.txt file '''
+#    with open(f'{part_id}/start_time.txt','w') as f:
+#        f.write(f'{start_time}')
+
+async def send_intro(update,context):
+    chat = await initialize_chat_handler(update,context)
+    chat.log(f'Chat {chat.chat_id} from {chat.name}: Sending first interaction prompt')
+    await chat.send_msg("""Hello! And welcome to an audio reflection experiment, brought to you by GEMH 🌺
+
+Please run /request to receive the audio for a value tension""")
+
+async def initialize_chat_handler(update,context=None):
+    chat_id = update.effective_chat.id
+    if chat_id not in chat_handlers:
+        chat_handlers[chat_id] = ChatHandler(chat_id,update,context)
+        chat = chat_handlers[chat_id]
+    chat = chat_handlers[chat_id]
+    return chat
+
+async def request(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''When we receive /start, start logging, say hi, and send voicenote back if it's a DM'''
+    chat = await initialize_chat_handler(update,context)
+    chat.log('Received /request command')
+    # Need to send back in order so will need to track what has been sent to the user
+    await chat.send_intro_vn()
+
+async def help_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''When we receive /help, start logging, say hi, and send voicenote back if it's a DM'''
+    chat = await initialize_chat_handler(update,context)
+    chat.log('Received /help command')
+    await chat.send_msg("""Thanks for submitting your reflections. If you have any trouble, feel free to explain the issue by typing a text message to me below and someone from Bloom will help.""")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    '''When we receive /start, start logging, say hi, and send voicenote back if it's a DM'''
+    chat = await initialize_chat_handler(update,context)
+    chat.log('Received /start command')
+    bot = context.bot
+    if 'group' in chat.chat_type: # To inlude both group and supergroup
+        admins = await bot.get_chat_administrators(chat.chat_id)
+        bot_admin = any(admin.user.id == bot.id for admin in admins)
+        if bot_admin:
+            await send_intro(update, context)
+        else:
+            await chat.send_msg("Please make me an admin of this group so that I can assist you 🥰")
+    else:
+        await send_intro(update, context)
+    #cmd = f'rclone copy --drive-shared-with-me -P 00_Participants bryankam8@gmail.com:"04_AUDIO PROTOTYPE_June 2023/00_Participants"'
+    #subprocess.check_output(cmd, shell=True)
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(DB)
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS logs (
+      timestamp INTEGER,
+      name TEXT,
+      chat_id INTEGER, 
+      filename TEXT
+    )""")
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    help_handler = CommandHandler('help', help_msg)
+    request_handler = CommandHandler('request', request)
+    voice_handler = MessageHandler(filters.VOICE , get_voice)
+    start_handler = CommandHandler('start', start)
+
+    application.add_handler(echo_handler)
+    application.add_handler(help_handler)
+    application.add_handler(request_handler)
+    application.add_handler(voice_handler)
+    application.add_handler(start_handler)
+
+    application.run_polling()
